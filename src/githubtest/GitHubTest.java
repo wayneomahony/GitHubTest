29bf8bf3e09cc616ae34a56a87cf925ad95df07b
wayneomahony <wayneomahony@gmail.com>
2015-10-22 13:36:35 +0100

    Waynes edit

--- src/githubtest/GitHubTest.java
+++ src/githubtest/GitHubTest.java
@@ -16,6 +16,7 @@ public class GitHubTest {
      */
     public static void main(String[] args) {
         System.out.println("Hello, Roft here!");
+        System.out.println("Wayne here..");
     }
     
 }
